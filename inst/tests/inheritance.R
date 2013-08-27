fd2e6b0f8f31823c4c65916a20fe339fb2038646
Carl Boettiger <cboettig@gmail.com>
2013-08-27 16:26:54 -0700

    Working on #1 : What works and what doesn't: The example in inst/tests/inheritance.R should run using the class definitions in R/inherited_classes.R.  Other class definitions are obsoleted by these updates and should eventually be removed. The ape methods should be unaffected, since the S4 classes have the same structure as before, only defined through inheritance instead.
    ToDo:
    - Remove the obsoleted methods in classes.R, nexml_read.R and nexml_write.R
    - Check the ape.R unit tests, update other unit tests
    - Write remaining essential classes out by hand based on
    inheritance and schema definitions: e.g. edge, otu, otus, tree, trees, nexml

--- inst/tests/inheritance.R
+++ inst/tests/inheritance.R
@@ -1,10 +1,11 @@
+
+#source("R/inherited_classes.R")
 require(XML)
 node <- newXMLNode("meta", 
                 attrs = c(id="dict1", property="cdao:has_tag",
 			                    content="true", 'xsi:type'="nex:LiteralMeta",
 			                    datatype="xsd:boolean"), suppressNamespaceWarning=TRUE)
 n2 <- newXMLNode("node", attrs = c(id = "n4", label="n4", about="#n4"), .children = node)
-
 
 s4 <- as(n2, "node")
 as(s4, "XMLInternalElementNode")
