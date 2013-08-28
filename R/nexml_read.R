ffb7342f1d28b351984685a83e11d7df488696e8
Carl Boettiger <cboettig@gmail.com>
2013-08-28 10:59:04 -0700

    start cleaning up old read/write methods obsoleted by toNeXML/fromNeXML methods

--- R/nexml_read.R
+++ R/nexml_read.R
@@ -13,14 +13,15 @@
 #' f <- system.file("examples", "trees.xml", package="RNeXML")
 #' nexml_read(f) 
 nexml_read <- function(x, type = c("phylo", "phylo4", "ouch",
-                                   "character_matrix", "nexml")){
+                                   "matrix", "nexml")){
   type <- match.arg(type) 
-  doc <- xmlParse(x)
+  doc <- xmlParse(x) 
   # will return class multiphylo if phylo is asked for and multiple trees are given.  
   as(doc, type)
 }
 
 
+# possibly not a good conversion to define
 setAs("XMLInternalDocument", "phylo", function(from){
   trees <- getNodeSet(from, "//nex:trees/nex:tree", namespaces="nex")
   out <- lapply(trees, as, "phylo")
@@ -33,41 +34,5 @@ setAs("XMLInternalDocument", "phylo", function(from){
   out
 })
 
-
-### Coercion methods from XML to S4 types, e.g. for reading XML ########## 
-
-setAs("XMLInternalElementNode", "meta", function(from) xmlToS4(from))
-setAs("XMLInternalElementNode", "node", function(from) xmlToS4(from))
-setAs("XMLInternalElementNode", "edge", function(from) xmlToS4(from))
-#setAs("XMLInternalElementNode", "node", function(from){
-#         obj = new("node")
-#         kids = xmlChildren(from)
-#         obj@meta = new("ListOfmeta", lapply(kids[names(kids) == "meta"], as, "meta"))
-#         ats = xmlAttrs(from, addNamespacePrefix = TRUE)
-#         for(i in names(ats))
-#            slot(obj, i) = ats[i]
-#         obj
-#       }
-#      )
-#setAs("XMLInternalElementNode", "edge", function(from) {
-#         obj = new("edge")
-#         kids = xmlChildren(from)
-#         obj@meta = new("ListOfmeta", lapply(kids[names(kids) == "meta"], as, "meta"))
-#         ats = xmlAttrs(from, addNamespacePrefix = TRUE)
-#         for(i in names(ats))
-#            slot(obj, i) = ats[i]
-#         obj
-#       })
-setAs("XMLInternalElementNode", "tree",
-       function(from) {
-         obj = new("tree")
-         kids = xmlChildren(from)
-         obj@edges = new("ListOfedge", lapply(kids[names(kids) == "edge"], as, "edge"))
-         obj@nodes = new("ListOfnode", lapply(kids[names(kids) == "node"], as, "node"))
-         ats = xmlAttrs(from, addNamespacePrefix = TRUE)
-         for(i in names(ats))
-            slot(obj, i) = ats[i]
-         obj
-       })
 
 
