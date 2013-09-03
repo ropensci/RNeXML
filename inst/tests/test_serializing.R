8f3af7782a37f2246b09f33614d99be8b31d9998
Carl Boettiger <cboettig@gmail.com>
2013-09-03 09:52:39 -0700

    validate against web schema, not local file

--- inst/tests/test_serializing.R
+++ inst/tests/test_serializing.R
@@ -20,8 +20,8 @@ test_that("We can serialize ape to S4 RNeXML into valid NeXML",{
   ###  Higher level API tests
   nexml_write(bird.orders, "test.xml")
 
-# results <- xmlSchemaValidate("http://www.nexml.org/2009/nexml.xsd", "test.xml")
-  results <- xmlSchemaValidate("~/Documents/code/thirdparty/nexml/xsd/nexml.xsd", "test.xml")
+ results <- xmlSchemaValidate("http://www.nexml.org/2009/nexml.xsd", "test.xml")
+#  results <- xmlSchemaValidate("~/Documents/code/thirdparty/nexml/xsd/nexml.xsd", "test.xml")
   expect_equal(results$status, 0)
   expect_equal(length(results$errors), 0)
 
