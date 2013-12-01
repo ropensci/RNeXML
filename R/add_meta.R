e1a88b44431058cfcf06ee55fc7f39ba3c7f5473
Carl Boettiger <cboettig@gmail.com>
2013-11-30 21:49:05 -0800

    added documentation, handling of namespace additions (including avoiding duplicates)

--- R/add_meta.R
+++ R/add_meta.R
@@ -1,19 +1,40 @@
 #' Add metadata to a nexml file
 #' 
 #' @param meta a meta S4 object, e.g. ouput of the function \code{\link{meta}}, or a list of these meta objects
-#' @param a nexml (S4) object
-#' @param level the level at which the metadata annotation should be added. 
+#' @param nexml (S4) object
+#' @param level the level at which the metadata annotation should be added.
+#' @param namespaces named character string for any additional namespaces that should be defined.  
 #' @param i for otus, trees, characters: if there are multiple such blocks, which one should be annotated?  Default is first/only block.  
 #' @param at_id the id of the element to be annotated.  Optional, advanced use only.  
-#' @seealso \code{\link{add_trees}} \code{\link{add_characters}}
+#' @seealso \code{\link{meta}} \code{\link{add_trees}} \code{\link{add_characters}} \code{\link{add_basic_meta}}
 #' @return the updated nexml object
 #' @examples
-#' modified <- meta(property = "prism:modificationDate",
-#'                 content = "2013-10-04")
-#'  add_meta(modified)
+#' ## Create a new nexml object with a single metadata element: 
+#' modified <- meta(property = "prism:modificationDate", content = "2013-10-04")
+#' nex <- add_meta(modified) # Note: 'prism' is defined in nexml_namespaces by default.  
+#' 
+#' ## Write multiple metadata elements, including a new namespace:  
+#' website <- meta(href = "http://carlboettiger.info", 
+#'                 rel = "foaf:homepage")              # meta can be link-style metadata
+#' nex <- add_meta(list(modified,  website), 
+#'                 namespaces = c(foaf = "http://xmlns.com/foaf/0.1/"))
+#'
+#' ## Append more metadata, and specify a level: 
+#' history <- meta(property = "skos:historyNote",
+#'                  content = "Mapped from the bird.orders data in the ape package using RNeXML")
+#' nex <- add_meta(history, 
+#'                 nexml = nex,
+#'                 level = "trees",
+#'                 namespaces = c(skos = "http://www.w3.org/2004/02/skos/core#"))
+#' 
 #' @export add_meta
 #' 
-add_meta <- function(meta, nexml=new("nexml"), level=c("nexml", "otus", "trees", "characters"), i = 1, at_id = NULL){
+add_meta <- function(meta, 
+                     nexml=new("nexml"), 
+                     level=c("nexml", "otus", "trees", "characters"), 
+                     namespaces = NULL,
+                     i = 1, 
+                     at_id = NULL){
   level <- match.arg(level)
   if(is(meta, "meta"))
     meta <- list(meta)
@@ -33,8 +54,18 @@ add_meta <- function(meta, nexml=new("nexml"), level=c("nexml", "otus", "trees",
     nexml@characters[[i]]@meta <- new("ListOfmeta", c(nexml@characters[[i]]@meta, meta))
   } 
 
+  ## append additional namespaces 
+  if(!is.null(namespaces)){
+    notdups <- match(namespaces, nexml@namespaces)
+    notdups <- sapply(notdups, is.na)
+    if(all(notdups)) # all are unique 
+      nexml@namespaces <-  c(nexml@namespaces, namespaces)
+    else {
+      nexml@namespaces <-  c(nexml@namespaces, namespaces[notdups])
+    } 
+  }
 
   nexml
-
 }
+
 
