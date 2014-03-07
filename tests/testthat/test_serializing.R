83b08a5fb5f22cf96b8ee035874f4746df0af75a
Carl Boettiger <cboettig@gmail.com>
2014-03-07 15:43:08 -0800

    use online schema, not local schema, to validate

--- tests/testthat/test_serializing.R
+++ tests/testthat/test_serializing.R
@@ -34,8 +34,8 @@ test_that("We can serialize parsed NeXML to S4 RNeXML into valid NeXML",{
 
 #! change directory to something that will work across machines if possible
   
-# results <- xmlSchemaValidate("http://www.nexml.org/2009/nexml.xsd", "test.xml")
-  results <- xmlSchemaValidate("~/Documents/code/thirdparty/nexml/xsd/nexml.xsd", "test.xml")
+ results <- xmlSchemaValidate("http://www.nexml.org/2009/nexml.xsd", "test.xml")
+#  results <- xmlSchemaValidate("~/Documents/code/thirdparty/nexml/xsd/nexml.xsd", "test.xml")
   expect_equal(results$status, 0)
   expect_equal(length(results$errors), 0)
 
