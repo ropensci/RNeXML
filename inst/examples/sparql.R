4e5dc14f80df76264d55df1856d1e06a80982832
Rutger Vos <rutgeraldo@gmail.com>
2014-07-04 23:24:22 +0200

    now nearly works, except for the leaning toothpick syndrome

--- inst/examples/sparql.R
+++ inst/examples/sparql.R
@@ -1,14 +1,15 @@
 library(rrdf)
+library(ape)
 
 # load the graph as extracted from primates.xml by RDFa2RDFXML.xsl
-graph <- load.rdf("/Users/rutger.vos/Documents/projects/RNeXML/inst/examples/primates_meta_xslt.xml")
+graph <- load.rdf("/Users/rvosa/Documents/projects/RNeXML/inst/examples/primates_meta_xslt.xml")
 
 # fetch the NCBI URI for the taxon that has rank 'Order', i.e. the root of the primates. The dot operator
 # '.' between clauses implies a join, in this case
 root <- sparql.rdf(graph, 
     "SELECT ?uri WHERE { 
-        ?x <http://rs.tdwg.org/ontology/voc/TaxonConcept#rank> <http://rs.tdwg.org/ontology/voc/TaxonRank#Order> . 
-        ?x <http://rs.tdwg.org/ontology/voc/TaxonConcept#toTaxon> ?uri    
+        ?id <http://rs.tdwg.org/ontology/voc/TaxonConcept#rank> <http://rs.tdwg.org/ontology/voc/TaxonRank#Order> . 
+        ?id <http://rs.tdwg.org/ontology/voc/TaxonConcept#toTaxon> ?uri    
     }"               
 )
 
@@ -23,13 +24,15 @@ recurse <- function(node) {
           }",
           sep=""
     );
-    result <- sparql.rdf(graph,rank_query);
+    result <- sparql.rdf(graph,rank_query);    
   
     # if rank is terminal, return the name
     if ( result[1] == "http://rs.tdwg.org/ontology/voc/TaxonRank#Species" ) {
-        return result[2];
+        name <- result[2];
+        name <- gsub("^.+#","",name,perl=TRUE);
+        return(name);
     }
-  
+    
     # recurse deeper
     else {
         child_query <- paste(
@@ -40,10 +43,10 @@ recurse <- function(node) {
             sep=""
         );
         children <- sparql.rdf(graph,child_query);
-        return paste( "(", paste( sapply( children, recurse ), sep="," ), ")", sep="");
+        return( paste( "(", paste( sapply( children, recurse ), sep="," ), ")", sep="" ) );
     }
 }
 
 # run it
 newick <- paste( recurse(root), ";", sep="" );
-plot(read.tree(text=newick));
+plot( read.tree( text=newick ) );
