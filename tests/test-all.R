c646d3ea5d34eb6e61ac852d2340384d9bb7a2c3
Carl Boettiger <cboettig@gmail.com>
2015-02-11 15:22:23 -0800

    move helper method for testthat tests to the appropriate place

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,13 +1,5 @@
-expect_true_or_null <- function(o){
-  if(!is.null(o)){
-    expect_true(o)
-  } else {
-    expect_null(o)
-  }
-}
-
 if (packageVersion("testthat") >= "0.7.1.99") {
-    library(testthat)
+  library(testthat)
   test_check("RNeXML")
 } else {
   library(testthat)
---
+++ tests/testthat/helper-RNeXML.R
@@ -0,0 +1,9 @@
+expect_true_or_null <- function(o){
+  if(!is.null(o)){
+    expect_true(o)
+  } else {
+    expect_null(o)
+  }
+}
+
+
