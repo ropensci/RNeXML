98d6c7ec417481bb34383c2a1313ab15b5950c83
Carl Boettiger <cboettig@gmail.com>
2015-04-28 14:02:38 -0700

    prebuilt vignettes

---
+++ inst/doc/S4.R
@@ -0,0 +1,49 @@
+## ----supplement-compile-settings, include=FALSE--------------------------
+library("methods")
+library("knitr")
+opts_chunk$set(tidy = FALSE, warning = FALSE, message = FALSE, 
+               cache = 1, comment = NA, verbose = TRUE)
+basename <- 'S4' 
+opts_chunk$set(fig.path = paste("figure/", basename, "-", sep=""),
+               cache.path = paste("cache/", basename, "/", sep=""))
+
+
+
+## ------------------------------------------------------------------------
+nex <- new("nexml")
+
+## ------------------------------------------------------------------------
+slotNames(nex)
+
+## ------------------------------------------------------------------------
+nex@version
+nex@namespaces
+
+## ------------------------------------------------------------------------
+class(nex@characters)
+
+## ------------------------------------------------------------------------
+length(nex@characters)
+
+## ------------------------------------------------------------------------
+nex@characters <- new("ListOfcharacters", list(new("characters")))
+
+## ------------------------------------------------------------------------
+length(nex@characters)
+
+## ------------------------------------------------------------------------
+class(nex@characters[[1]])
+
+## ------------------------------------------------------------------------
+f <- system.file("examples", "trees.xml", package="RNeXML")
+nex <- nexml_read(f)
+
+## ------------------------------------------------------------------------
+nex@otus[[1]]@otu[[1]]@label
+
+## ------------------------------------------------------------------------
+nex@otus[[1]]@otu[[1]]@meta <- 
+  c(meta("skos:note", 
+          "This species was incorrectly identified"),
+         nex@otus[[1]]@otu[[1]]@meta)
+
