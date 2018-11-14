f90e9f3493f71e5f813962d399365aa2f5bc2703
Hilmar Lapp <hlapp@drycafe.net>
2018-11-13 21:06:40 -0500

    Tests for reading, returning, and serializing XML literal metas

--- tests/testthat/test_serializing.R
+++ tests/testthat/test_serializing.R
@@ -33,7 +33,23 @@ test_that("We can serialize parsed NeXML to S4 RNeXML into valid NeXML",{
 
   })
 
+test_that("We can correctly serialize XML literals as metadata", {
+  nex1 <- read.nexml(system.file("examples", "phenex.xml", package="RNeXML"))
+  m_xml1 <- get_metadata(nex1, "characters/format/states/state") %>%
+    dplyr::filter(property == "ps:describesPhenotype")
+  # write it out and then read it back in
+  write.nexml(nex1, file="test.xml")
+  expect_true_or_null(nexml_validate("test.xml"))
+  nex2 <- read.nexml("test.xml")
+  m_xml2 <- get_metadata(nex2, "characters/format/states/state") %>%
+    dplyr::filter(property == "ps:describesPhenotype")
+  # the XML literals in the form of string values should be the same
+  testthat::expect_true(all(dplyr::arrange(m_xml1, state)[,"content"] ==
+                              dplyr::arrange(m_xml2, state)[,"content"]))
 
+  ## clean up
+  unlink("test.xml")
+})
 
 #root <- xmlRoot(xmlParse(system.file("examples", "trees.xml", package="RNeXML")))
 #tree <- as(root, "nexml")
