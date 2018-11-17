41d254aa2f7dd1e5301ec9927f2682650fe9a053
Hilmar Lapp <hlapp@drycafe.net>
2018-11-16 19:23:59 -0500

    Makes validation test error much more informative

--- tests/testthat/helper-RNeXML.R
+++ tests/testthat/helper-RNeXML.R
@@ -1,8 +1,9 @@
 expect_true_or_null <- function(o){
+  val <- rlang::enquo(o)
   if(!is.null(o)){
-    expect_true(o)
+    expect_true(!!val)
   } else {
-    expect_null(o)
+    expect_null(!!val)
   }
 }
 
