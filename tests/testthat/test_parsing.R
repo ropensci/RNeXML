98a04694daed8f35e3547902893ff2f30093ed9c
Hilmar Lapp <hlapp@drycafe.net>
2018-11-25 15:48:26 -0500

    Fixes namespaces from parsed NeXML not taking precedence
    Also adds tests to ensure this is the case.

--- tests/testthat/test_parsing.R
+++ tests/testthat/test_parsing.R
@@ -14,18 +14,30 @@ test_that("We can parse a NeXML file to an S4 RNeXML::tree object", {
 
 test_that("We preserve existing namespace", {
 
-  f <- system.file("examples/biophylo.xml", package="RNeXML")
+  f <- system.file("examples", "biophylo.xml", package="RNeXML")
   nex <- nexml_read(f)
+
+  expect_gt(length(get_namespaces(nex)), length(get_namespaces(nexml())))
+  ## Check that the new abbreviations are added
+  expect_true(all(c("concept", "map") %in% names(get_namespaces(nex))))
+
   g <- tempfile()
   nexml_write(nex, g)
-
   expect_true_or_null(nexml_validate(g))
 
   nex2 <- nexml_read(g)
-
-  ## check the namespaces are added 
-  expect_gt(length(get_namespaces(nex2)), length(get_metadata(nex)))
-
+  ## check the namespaces remain there
+  expect_gt(length(get_namespaces(nex2)), length(get_namespaces(nexml())))
   ## Check that the new abbreviations are added 
+  expect_true(all(c("concept", "map") %in% names(get_namespaces(nex2))))
+  expect_equal(get_namespaces(nex2)["concept"], get_namespaces(nex)["concept"])
+  expect_equal(get_namespaces(nex2)["map"], get_namespaces(nex)["map"])
 
+  f <- system.file("examples", "phenoscape.xml", package = "RNeXML")
+  nex <- nexml_read(f)
+  # check that the cdao namespace didn't get clobbered
+  expect_true("cdao" %in% names(get_namespaces(nex)))
+  expect_equivalent(get_namespaces(nex)["cdao"],
+                    "http://www.evolutionaryontology.org/cdao/1.0/cdao.owl#")
+  expect_true(get_namespaces(nex)["cdao"] != get_namespaces(nexml())["cdao"])
 })
